201b39d8499c6f89e77529813057ea088cace93a
Luiz Ribeiro <luizribeiro@gmail.com>
2023-01-16 16:02:35 -0500

    few tweaks

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@ extern crate pest;
 #[macro_use]
 extern crate pest_derive;
 
+use pest::iterators::Pair;
 use pest::Parser;
 use std::fs;
 
@@ -13,9 +14,21 @@ fn main() {
     let unparsed_file = fs::read_to_string("example.shady").unwrap();
     let pairs = ShadyParser::parse(Rule::program, &unparsed_file).unwrap();
 
-    for pair in pairs {
+    fn parse_value(pair: Pair<Rule>) {
+        println!("-----------");
         println!("Rule: {:?}", pair.as_rule());
         println!("Span: {:?}", pair.as_span());
         println!("Text: {}", pair.as_str());
+        match pair.as_rule() {
+            _ => {
+                for pair in pair.into_inner() {
+                    parse_value(pair);
+                }
+            }
+        }
+    }
+
+    for pair in pairs {
+        parse_value(pair);
     }
 }
