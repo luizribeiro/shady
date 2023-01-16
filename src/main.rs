18aa811805ccb3e39acdeb1e6a0aca006f2a2a57
Luiz Ribeiro <luizribeiro@gmail.com>
2023-01-16 15:19:36 -0500

    primitive parser from calculator

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
+extern crate pest;
+#[macro_use]
+extern crate pest_derive;
+
+use pest::Parser;
+use std::fs;
+
+#[derive(Parser)]
+#[grammar = "shady.pest"]
+pub struct ShadyParser;
+
 fn main() {
-    println!("Hello, world!");
+    let unparsed_file = fs::read_to_string("example.shady").unwrap();
+    let pairs = ShadyParser::parse(Rule::program, &unparsed_file).unwrap();
+
+    for pair in pairs {
+        println!("Rule: {:?}", pair.as_rule());
+        println!("Span: {:?}", pair.as_span());
+        println!("Text: {}", pair.as_str());
+    }
 }
