a7c346de329fb2db9980daec7f40d486193c1a7b
Luiz Ribeiro <luizribeiro@gmail.com>
2023-01-21 08:34:20 -0500

    moved cli utils from main.rs to cli.rs

--- src/cli.rs
+++ src/cli.rs
@@ -1,4 +1,7 @@
-use crate::ast::Type;
+use std::collections::HashMap;
+
+use crate::ast;
+use crate::eval;
 use crate::eval::ShadyContext;
 
 use clap::{command, value_parser, Arg, Command};
@@ -7,7 +10,7 @@ fn string_to_static_str(s: String) -> &'static str {
     Box::leak(s.into_boxed_str())
 }
 
-pub fn get_command(context: &ShadyContext) -> clap::Command {
+fn get_command(context: &ShadyContext) -> clap::Command {
     let mut cmd = command!().bin_name(format!("shady {}", context.filename));
 
     for fun in &context.program.fn_definitions {
@@ -21,9 +24,9 @@ pub fn get_command(context: &ShadyContext) -> clap::Command {
             let mut arg = Arg::new(string_to_static_str(param.name.to_string()));
             arg = arg.required(true);
             arg = match param.typ {
-                Type::Int => arg.value_parser(value_parser!(i64)),
-                Type::Str => arg.value_parser(value_parser!(String)),
-                Type::Bool => arg.value_parser(value_parser!(bool)),
+                ast::Type::Int => arg.value_parser(value_parser!(i64)),
+                ast::Type::Str => arg.value_parser(value_parser!(String)),
+                ast::Type::Bool => arg.value_parser(value_parser!(bool)),
             };
             subcmd = subcmd.arg(arg);
         }
@@ -31,4 +34,33 @@ pub fn get_command(context: &ShadyContext) -> clap::Command {
     }
 
     return cmd;
+}
+
+pub fn run_fn(context: &ShadyContext, script_args: &Vec<String>) {
+    let cmd = get_command(&context);
+    let matches = cmd.get_matches_from(&*script_args);
+
+    let mut local_context = eval::LocalContext {
+        vars: HashMap::new(),
+    };
+    let subcmd_name = matches.subcommand_name().unwrap_or("main");
+    let fun = ast::get_fn_by_name(&context.program, subcmd_name).unwrap();
+    let args = matches.subcommand_matches(&subcmd_name);
+    if let Some(args) = args {
+        for param in &fun.signature.parameters {
+            args.get_raw(&param.name)
+                .unwrap()
+                .for_each(|raw_cli_value| {
+                    let cli_value: String = raw_cli_value.to_string_lossy().into_owned();
+                    let value = match param.typ {
+                        ast::Type::Str => ast::Value::String(cli_value),
+                        ast::Type::Int => ast::Value::Int(cli_value.parse().unwrap()),
+                        ast::Type::Bool => ast::Value::Bool(cli_value.parse().unwrap()),
+                    };
+                    local_context.vars.insert(param.name.clone(), value);
+                });
+        }
+    }
+
+    eval::eval_expr(&local_context, &context, &fun.expr);
 }
--- src/main.rs
+++ src/main.rs
@@ -8,7 +8,6 @@ mod eval;
 
 use clap::Parser;
 use eval::ShadyContext;
-use std::collections::HashMap;
 
 #[derive(Parser, Debug)]
 #[command(author, version, about, long_about = None)]
@@ -36,33 +35,8 @@ fn main() {
         return;
     }
 
-    let cmd = cli::get_command(&context);
-
     let mut script_args = vec![context.filename.clone()];
     script_args.extend(args.args.clone());
-    let matches = cmd.get_matches_from(&script_args);
 
-    let mut local_context = eval::LocalContext {
-        vars: HashMap::new(),
-    };
-    let subcmd_name = matches.subcommand_name().unwrap_or("main");
-    let fun = ast::get_fn_by_name(&context.program, subcmd_name).unwrap();
-    let args = matches.subcommand_matches(&subcmd_name);
-    if let Some(args) = args {
-        for param in &fun.signature.parameters {
-            args.get_raw(&param.name)
-                .unwrap()
-                .for_each(|raw_cli_value| {
-                    let cli_value: String = raw_cli_value.to_string_lossy().into_owned();
-                    let value = match param.typ {
-                        ast::Type::Str => ast::Value::String(cli_value),
-                        ast::Type::Int => ast::Value::Int(cli_value.parse().unwrap()),
-                        ast::Type::Bool => ast::Value::Bool(cli_value.parse().unwrap()),
-                    };
-                    local_context.vars.insert(param.name.clone(), value);
-                });
-        }
-    }
-
-    eval::eval_expr(&local_context, &context, &fun.expr);
+    cli::run_fn(&context, &script_args);
 }
