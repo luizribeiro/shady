ba45ab4538f8857ac372f316aff7fe4132d63805
Luiz Ribeiro <luizribeiro@gmail.com>
2023-01-27 22:14:52 -0500

    few more builtins

--- src/builtins/proc.rs
+++ src/builtins/proc.rs
@@ -2,17 +2,33 @@ use crate::types::Value;
 use shady_macros::builtin;
 use std::process::Command;
 
-#[builtin]
-fn exec(proc: Value) -> i64 {
+fn command(proc: Value) -> Command {
     let (program, args) = match proc {
         Value::Proc { program, args } => (program, args),
         _ => panic!("expected proc"),
     };
 
-    Command::new(program)
-        .args(args)
+    let mut command = Command::new(program);
+    command.args(args);
+    command
+}
+
+#[builtin]
+fn exec(proc: Value) -> i64 {
+    command(proc)
         .status()
         .expect("failed to execute process")
         .code()
         .unwrap_or(0) as i64
+}
+
+#[builtin]
+fn stdout(proc: Value) -> String {
+    String::from_utf8(
+        command(proc)
+            .output()
+            .expect("failed to execute process")
+            .stdout,
+    )
+    .unwrap()
 }
--- src/builtins/str.rs
+++ src/builtins/str.rs
@@ -10,6 +10,11 @@ fn str_neq_str(a: String, b: String) -> bool {
     a != b
 }
 
+#[builtin]
+fn lines(s: String) -> Vec<String> {
+    s.lines().map(|s| s.to_string()).collect()
+}
+
 #[cfg(test)]
 mod test {
     use super::*;
@@ -24,5 +29,13 @@ mod test {
     fn test_str_neq_str() {
         assert_eq!(str_neq_str("a".to_string(), "a".to_string()), false);
         assert_eq!(str_neq_str("a".to_string(), "b".to_string()), true);
+    }
+
+    #[test]
+    fn test_lines() {
+        assert_eq!(
+            lines("a\nb\nc".to_string()),
+            vec!["a".to_string(), "b".to_string(), "c".to_string()]
+        );
     }
 }
